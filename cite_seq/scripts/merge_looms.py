e91252e3b52d4aa3b97fa48df9e318d5f7f5111c
Garth Kong <kongga2017@gmail.com>
2022-05-10 16:06:00 -0700

    fix repeated folder structure

---
+++ cite_seq/scripts/merge_looms.py
@@ -0,0 +1,12 @@
+import os
+import loompy
+
+####################
+# GLOBAL VARIABLES #
+####################
+input_files = snakemake.input.input_list #["file1.loom","file2.loom", ... ]
+output_filename = snakemake.output[0]
+
+#combine loom files
+loompy.combine(files = input_files, output_file = str(output_filename), key = 'Accession')
+
